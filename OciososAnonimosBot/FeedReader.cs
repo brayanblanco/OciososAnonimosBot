70a48831c400b4954b10d794b30db4c0d46dd499
Aqueloutrado <brayan.martin.blanco@gmail.com>
2017-12-28 14:46:50 +0000

    Added FeedReader class to read the blog

---
+++ OciososAnonimosBot/FeedReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace OciososAnonimosBot {
+    public class FeedReader {
+
+        private string _uri;
+
+        public FeedReader(string uri) {
+            _uri = uri;
+        }
+        public string Read() {
+            using (XmlReader reader = XmlReader.Create(_uri)) {
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                return feed.Links[0].Uri.ToString();                
+            }
+        }
+
+    }
+}
